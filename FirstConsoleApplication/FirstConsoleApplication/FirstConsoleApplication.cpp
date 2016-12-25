026603d9bb4125e4985651191f9ee7dc5d2c253d
chinm <tq.nmchi@gmail.com>
2016-12-25 23:50:39 +0700

    add first project
    add first project

---
+++ FirstConsoleApplication/FirstConsoleApplication/FirstConsoleApplication.cpp
@@ -0,0 +1,12 @@
+// FirstConsoleApplication.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+
+int main()
+{
+	printf("Hello World C++\n");
+    return 0;
+}
+
